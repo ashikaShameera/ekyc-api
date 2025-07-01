2ec7086a1ed99a29883ebeb5a2b9f48d043416e7
ashikaShameera <ashikashameera@gmail.com>
2025-07-01 17:34:13 +0530

    final commit otp

--- src/utils/generateOtp.js
+++ src/utils/generateOtp.js
@@ -2,7 +2,7 @@ import crypto from 'crypto';
 
 export function generateNumericOtp (digits = 6) {
   const max = 10 ** digits;
-  //let otp = crypto.randomInt(0, max).toString().padStart(digits, '0');
+  let otp = crypto.randomInt(0, max).toString().padStart(digits, '0');
   // let otp="111111"
   console.log("**********************************************************************************************************")
   console.log("**********************************************************************************************************")
